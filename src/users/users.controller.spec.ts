6017de8ed32dd8f6fc60b53d1bf3591856e427ff
Masihbalali <Masihbalali@outlook.com>
2024-09-06 21:45:10 +0330

    fiX: controler.

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 
+// User controller 
 describe('UsersController', () => {
   let controller: UsersController;
 
